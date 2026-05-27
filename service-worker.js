6b3f79148f581213ded11bc4d0f76c613a4842fe
sangshin park <parkss9799@gmail.com>
2026-05-27 21:21:52 +0900

    PWA 설정 추가

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "sobagongbang-pyeongchon-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./store-manager.html",
+  "./employee-info.html",
+  "./daily-worker.html",
+  "./store-manager.css",
+  "./store-manager.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
