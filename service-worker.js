57bf72ab929234bbd8235272f4d9cf1561fcfacf
thebigkorea <thebig@koreafnb.com>
2026-05-28 19:15:30 +0900

    서비스워커 캐시 비활성화

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,15 @@
-const CACHE_NAME = "sobagongbang-pyeongchon-v2";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./store-manager.html",
-  "./employee-info.html",
-  "./daily-worker.html",
-  "./store-manager.css",
-  "./store-manager.js"
-];
-
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
